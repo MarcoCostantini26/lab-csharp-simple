ea5f4b6d662b4a08f1c5138bdca8381e60a3a8eb
status <marco.costantini7@studio.unibo.it>
2021-12-13 11:51:15 +0100

    Complete ex02

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Reflection.Metadata;
+
 namespace ComplexAlgebra
 {
     /// <summary>
@@ -13,10 +16,55 @@ namespace ComplexAlgebra
     /// TODO: * summing up or subtracting two complex numbers
     /// TODO: * representing a complex number as a string or the form Re +/- iIm
     /// TODO:     - e.g. via the ToString() method
+    /// 
     /// TODO: * checking whether two complex numbers are equal or not
     /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public Complex(double realPart, double imaginaryPart)
+        {
+            this.Real = realPart;
+            this.Imaginary = imaginaryPart;
+        }
+
+        public double Real { get; }
+        
+        public double Imaginary { get; }
+
+        public double Modulus => Math.Sqrt(Math.Pow(2, Real) + Math.Pow(2, Imaginary));
+        
+        public double Phase => Math.Atan2(Imaginary, Real);
+
+        public Complex Complement() => new Complex(Real, -Imaginary);
+
+        public Complex Plus(Complex complexToSum) => new Complex(Real + complexToSum.Real,
+            Imaginary + complexToSum.Imaginary);
+        
+        public Complex Minus(Complex complexToSum) => new Complex(Real - complexToSum.Real,
+            Imaginary + complexToSum.Imaginary);
+
+        public override string ToString()
+        {
+            var sign = Imaginary > 0 ? "+" : "-";
+            return $"{Real} {sign} i{Math.Abs(Imaginary)}";
+        }
+
+        protected bool Equals(Complex other)
+        {
+            return Real.Equals(other.Real) && Imaginary.Equals(other.Imaginary);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Complex) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Real, Imaginary);
+        }
     }
 }
